ad1dd580117f900c6cc268865eb28ab3b3ed18d9
Tiago A Silva <tiago@tiago.blog.br>
2019-02-27 17:29:08 -0300

    Adicionar arquivos de projeto.

---
+++ ConverterRGBtoLAB/region_growing_lab.cpp
@@ -0,0 +1,34 @@
+
+
+#include "stdafx.h"
+/*
+#include "region_growing_lab.h"
+
+
+
+int RegionGrowingLAB::setInputLab(int i) {
+
+	return i;
+};
+
+
+void RegionGrowingLAB::setInputCloud(pcl::PointCloud<pcl::PointXYZLAB>::Ptr cloud) {
+
+	//return i;
+};
+
+
+
+
+
+float RegionGrowingLAB::calculateColorimetricalDifference(std::vector<float>& first_color, std::vector<float>& second_color) const) {
+	
+	float difference = 0.0f;
+	
+	difference += float((first_color[0] - second_color[0]) * (first_color[0] - second_color[0]));
+	difference += float((first_color[1] - second_color[1]) * (first_color[1] - second_color[1]));
+	difference += float((first_color[2] - second_color[2]) * (first_color[2] - second_color[2]));
+	
+	return (difference);
+}
+*/
